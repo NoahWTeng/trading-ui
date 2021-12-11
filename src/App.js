9e48c21f6d694e6c049c8ed8ad6aa8f4bef69efa
jirohero <noah.8station@gmail.com>
2021-12-11 18:11:41 +0100

    [app] base setting

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// import { I18nProvider } from '@lingui/react';
+// import { i18n } from '@lingui/core';
+
+// import { I18nRoutes } from '@services';
+
+// import { SEO } from '@components';
+
+export function App() {
+  // const { language, messages } = useSelector(s => s.language);
+
+  // i18n.load(language, messages);
+  // i18n.activate(language);
+
+  return (
+    <>
+      Hello World!
+      {/* <SEO />
+      <Router>
+        <I18nProvider i18n={i18n}>
+          <I18nRoutes />
+        </I18nProvider>
+      </Router> */}
+    </>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Redux from 'react-redux';
+import FastClick from 'fastclick'; // 延迟300ms再响应点击，目的是在移动端设备上检测用户是否在做双击
+
+// import { store } from './redux';
+import { App } from './App';
+
+// import '@styles/base.scss';
+// import '@helpers/rem';
+// import 'antd/dist/antd.css';
+// import 'antd-mobile/dist/antd-mobile.css';
+
+import * as serviceWorker from './registerServiceWorker';
+
+FastClick.attach(document.body);
+
+ReactDOM.render(
+  // <Redux.Provider store={store}>
+  <App />,
+  // </Redux.Provider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
